6ea354c8aa7f0b05e6c71236119fa72ed9eae18d
Jimmy Potekev <jimalpo1@gmail.com>
2026-04-13 14:55:37 -0700

    remove ABC pattern from camera manager and implement its methods

--- python_app/camera.py
+++ python_app/camera.py
@@ -1,5 +1,5 @@
 # General
-from multiprocessing import Process, Queue
+from multiprocessing import Process, Queue, Semaphore
 from abc import ABC, abstractmethod
 # Components
 from .cv_model import CVModel
@@ -12,14 +12,49 @@ logger = getLogger(__name__)
 ################################################################################
 # CAMERA WORKER - Performs image capturing and interfacing with the CV model   #
 ################################################################################
+class CameraWorkerIntf(ABC):
 
-class CameraWorker:
+    @abstractmethod
+    def dog_in_frame(self) -> None:
+        pass
+
+    @abstractmethod
+    def _get_frame(self):
+        pass
+
+    @abstractmethod
+    def _process_frame(self):
+        pass
+
+
+class RealCameraWorker(CameraWorkerIntf):
     def __init__(self) -> None:
-        logger.info("Initializing Camera")
+        logger.info("Initializing RealCameraWorker")
 
         self.model: CVModel = CVModel()
 
-        logger.info("Camera setup complete")
+        logger.info("RealCameraWorker setup complete")
+
+
+    def dog_in_frame(self) -> bool:
+        return False
+
+
+    def _get_frame(self):
+        pass
+
+
+    def _process_frame(self):
+        pass
+
+
+class FakeCameraWorker(CameraWorkerIntf):
+    def __init__(self) -> None:
+        logger.info("Initializing FakeCameraWorker")
+
+        # self.model: CVModel = CVModel()
+
+        logger.info("FakeCameraWorker setup complete")
 
 
     def dog_in_frame(self) -> bool:
@@ -38,6 +73,7 @@ class CameraWorker:
 #################################################################################
 
 def camera_process_main(in_queue: Queue, out_queue: Queue) -> None:
+    # TODO: need a way to configure RealCameraWorker or FakeCameraWorker based on mode. 
     camera = CameraWorker()
 
     while True:
@@ -56,65 +92,20 @@ def camera_process_main(in_queue: Queue, out_queue: Queue) -> None:
 ################################################################################
 # CAMERA MANAGER - Wrapper class for communicating with the camera process     #
 ################################################################################
-# NOTE: might not need an interface for the camera manager as it does not contain or represent a hardware component. 
-# the camera worker is the actual camera hardware. this would need an interface instead. 
-class CameraManagerIntf(ABC):
-    
-# PUBLIC
-    @abstractmethod
-    def start(self) -> None:
-        pass
 
-    @abstractmethod
-    def join(self) -> None:
-        pass
-    
-    @abstractmethod
-    def turn_on_camera(self) -> None:
-        pass
-
-    @abstractmethod
-    def turn_off_camera(self) -> None:
-        pass
-
-    @abstractmethod
-    def is_camera_on(self) -> bool:
-        pass
-
-    @abstractmethod
-    def dog_in_frame(self) -> bool:
-        pass
-
-    @abstractmethod
-    def sleep_until_timeout(self) -> None:
-        pass
-
-    @abstractmethod
-    def wait_for_timeout(self) -> None:
-        pass
-
-# PRIVATE
-    @abstractmethod
-    def _send_command(self, cmd: CameraComm):
-        pass
-
-    @abstractmethod
-    def _get_response(self) -> CameraComm:
-        pass
-
-
-class RealCameraManager(CameraManagerIntf):
+class CameraManager:
     def __init__(self) -> None:
         logger.info("Initializing CameraManager")
         
-        self.in_queue: Queue = Queue()
+        self.in_queue: Queue = Queue(maxsize=1)
         self.out_queue = Queue()
+        self.timeout_sem = Semaphore(value=0)
         self.process: Process = Process(
             target=camera_process_main,
-            args=(self.in_queue, self.out_queue),
+            args=(self.in_queue, self.out_queue, self.timeout_sem),
             daemon=True
         )
-        # TODO: add camera_is_on attribute
+        self.camera_is_on: bool = False
 
         logger.info("CameraManager setup complete")
 
@@ -128,90 +119,46 @@ class RealCameraManager(CameraManagerIntf):
 
     
     def turn_on_camera(self):
-        # debug log if camera is already on
-        pass
+        """Turns on the camera by waking the camera process. When the camera process is on, it is continuously executing commands sent to it.
+        Waits when no commands are being sent in the input queue."""
+        if self.camera_is_on:
+            logger.debug("Camera is already on")
+            return
+        self.in_queue.put(CameraComm.CAMERA_ON)
+        self.camera_is_on = True
 
 
     def turn_off_camera(self):
-        # debug log if camera is already off
-        pass
+        "Turns off the camera by putting the camera process to sleep."
+        if not self.camera_is_on:
+            logger.debug("Camera is already off")
+            return
+        self.in_queue.put(CameraComm.CAMERA_OFF)
+        self.camera_is_on = False
+
 
     def is_camera_on(self) -> bool:
-        return False
+        return self.camera_is_on
 
 
     def dog_in_frame(self) -> bool:
-        pass
+        # this check may be too simple. Assumes the camera process will only ever output DOG_FOUND or NO_DOG_FOUND.
+        return self.out_queue.get() == CameraComm.DOG_FOUND
 
 
     def sleep_until_timeout(self) -> None:
-        pass
+        # sends command for the camera process to sleep
+        self.in_queue.put(CameraComm.SLEEP_TIMEOUT)
 
 
     def wait_for_timeout(self) -> None:
-        pass
+        # waits for the camera process to post the timeout semaphore
+        self.timeout_sem.acquire()
 
 # PRIVATE
-    def _send_command(self, cmd: CameraComm):
-        pass
+    # These are not needed for now. They don't add anything. May expand later.
+    # def _send_command(self, cmd: CameraComm):
+    #     pass
 
-
-    def _get_response(self) -> CameraComm:
-        pass
-
-
-class FakeCameraManager(CameraManagerIntf):
-    def __init__(self) -> None:
-        logger.info("Initializing CameraManager")
-        
-        self.in_queue: Queue = Queue()
-        self.out_queue = Queue()
-        self.process: Process = Process(
-            target=camera_process_main,
-            args=(self.in_queue, self.out_queue),
-            daemon=True
-        )
-        # TODO: add camera_is_on attribute
-
-        logger.info("CameraManager setup complete")
-
-# PUBLIC
-    def start(self):
-        self.process.start()
-
-
-    def join(self):
-        self.process.join()
-
-    
-    def turn_on_camera(self):
-        # debug log if camera is already on
-        pass
-
-
-    def turn_off_camera(self):
-        # debug log if camera is already off
-        pass
-
-    def is_camera_on(self) -> bool:
-        return False
-
-
-    def dog_in_frame(self) -> bool:
-        pass
-
-
-    def sleep_until_timeout(self) -> None:
-        pass
-
-
-    def wait_for_timeout(self) -> None:
-        pass
-
-# PRIVATE
-    def _send_command(self, cmd: CameraComm):
-        pass
-
-
-    def _get_response(self) -> CameraComm:
-        pass
+    # def _get_response(self) -> CameraComm:
+    #     pass
--- python_app/enums.py
+++ python_app/enums.py
@@ -14,9 +14,11 @@ class ServoNum(Enum):
     SERVO4 = auto()
 
 class CameraComm(Enum):
-    CAPTURE_IMG = auto()
-    POWERUP = auto()        # 
-    SHUTDOWN = auto()       #
-    DOG_FOUND = auto()      # 
-    NO_DOG_FOUND = auto()   # signals to the main process that a dog was not found in the image
+    # OUTGOING MESSAGES
+    CAPTURE_IMG = auto()    # tells the camera process to analyze a frame
+    CAMERA_ON = auto()      # wakes the camera process
+    CAMERA_OFF = auto()     # puts the camera process to sleep until woken
     SLEEP_TIMEOUT = auto()  # signals the camera process to sleep, pausing object detection until timeout
+    # INCOMING MESSAGES
+    DOG_FOUND = auto()      # signals ot the main process that a dog was found in the image
+    NO_DOG_FOUND = auto()   # signals to the main process that a dog was not found in the image
